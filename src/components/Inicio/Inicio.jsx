2ee37af97e9e7fa04709d84881ac112601ba0755
Edu980-8 <daniedu151@gmail.com>
2023-02-21 18:37:51 -0500

    Arreglos de Lista de salida

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -6,12 +6,11 @@ import {
 } from "../../reducers/reducerCompra";
 import Carro_Item from "../Carro_Item/Carro_Item";
 import TarjetaProductos from "../Tarjeta_Productos/tarjetaProductos";
-import Ventas from "../Ventana_Venta/VentanaVenta";
  
 
 const Inicio = () => {
   const [state, dispatch] = useReducer(reducerCompra, estadoInicialCompra);
-  const { products, cart, totalQuantity } = state;
+  const { products, cart} = state;
 
   const addToCart = (id) => {
     dispatch({ type: TYPES.AGREGAR_CARRITO, payload: id });
@@ -28,18 +27,14 @@ const Inicio = () => {
 
   return (
     <div>
-      
       <a href="/ventas">
         <button>
           <p>{state.totalQuantity}</p>
           <img className="imagenCarrito" src="/src/components/Imagenes/Carrito.png"></img>
         </button>
       </a>
-        
       <h2 className="tituloInicio">Nuestros Servicios</h2>
-
       <div className="contenedorProductos">
-        
         {products.map((product) => (
           <TarjetaProductos
             key={product.id}
@@ -47,16 +42,11 @@ const Inicio = () => {
             addToCart={addToCart}
           />
         ))} {/* Renderiza la parte inicial de las tarjetas */}
-
-
         <div className="Lista_Productos">
           <button onClick={clearCart}>Limpiar Carrito</button>
           {cart.map((item, index) => (
             <Carro_Item key={index} data={item} delFromCart={delFromCart} />
           ))}
-
-
-
         </div>
       </div>
     </div>
--- src/components/Ventana_Venta/VentanaVenta.jsx
+++ src/components/Ventana_Venta/VentanaVenta.jsx
@@ -1,12 +1,33 @@
-const cart = JSON.parse(localStorage.getItem('cart'));
-const Ventas = (props) => { 
+const info = localStorage.getItem("cart");
+const cart = JSON.parse(localStorage.getItem("cart"));
+const cantidad_productos = cart.length
+const listaNombres = cart.map((producto) =>
+<li key={producto.id}>{producto.titulo } x {producto.cantidad} - Precio: {producto.precio * producto.cantidad}</li>
+);
+
+
+const Ventas = (props) => {
+  
+  function calculaTotal(cantidad_productos, precio){
+    var result = cantidad_productos * precio;
+    return result;
+  }
+
+
+  var total = 0;
+  for(let i = 0;i< cantidad_productos;i++){
+    total = total + calculaTotal(cart[i].cantidad, cart[i].precio)
+  }
+  console.log(total)
   return (
-    
-//   <div>
-//       <h2>Mi Ventana de Venta</h2>
-//       <div></div>
-//     </div>
-    console.log(cart)
+    <div>
+      <p className="parrafo_Total">Total: {total}</p>
+      
+        <h3>Carrito de Compras</h3>
+      <div className="lista_Salida">
+        {listaNombres}
+      </div>
+    </div>
   );
 };
 
