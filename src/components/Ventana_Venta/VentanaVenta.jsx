2bf0141540d0e4177ce7c057cb287a0dc317e82a
Edu980-8 <daniedu151@gmail.com>
2023-02-28 19:37:48 -0500

    Agregando Botones de redes.

--- src/components/Ventana_Venta/VentanaVenta.jsx
+++ src/components/Ventana_Venta/VentanaVenta.jsx
@@ -1,37 +1,45 @@
-const cart = JSON.parse(localStorage.getItem("cart")); //Creacion de un objeto a partir de una cadena
-if (cart) {
-  var cantidad_productos = cart.length;
+var cadena = localStorage.getItem("cart");
+var cart;
+console.log("Cadena:", cadena);
+console.log("Carrito", cart);
+var listaNombres;
+if (cadena) {
+  cart = JSON.parse(cadena); //Creacion de un objeto a partir de una cadena
+} else {
+  cart = [];
 }
 
-const listaNombres = cart.map((producto) =>
-<li key={producto.id}> 
-  {producto.titulo } x {producto.cantidad} - Precio: {producto.precio * producto.cantidad}
-<img  src={producto.imagen}></img>
-</li>
-);
+if (cart.length === 0) {
+  listaNombres = <p>No hay productos en el carrito.</p>;
+} else {
+  listaNombres = cart.map((producto) => (
+    <li key={producto.id}>
+      {producto.titulo} x {producto.cantidad} - Precio:
+      {producto.precio * producto.cantidad}
+      <img src={producto.imagen}></img>
+    </li>
+  ));
+}
+
 
 
 const Ventas = () => {
-  
-  function calculaTotal(cantidad_productos, precio){
+  function calculaTotal(cantidad_productos, precio) {
     var result = cantidad_productos * precio;
     return result;
   }
 
-
   var total = 0;
-  for(let i = 0;i< cantidad_productos;i++){
-    total = total + calculaTotal(cart[i].cantidad, cart[i].precio)
+  for (let i = 0; i < cart.length; i++) {
+    total = total + calculaTotal(cart[i].cantidad, cart[i].precio);
   }
-  console.log(total)
+  console.log(total);
   return (
     <div>
       <p className="parrafo_Total">Total: {total}</p>
-      
-        <h3>Carrito de Compras</h3>
-      <div className="lista_Salida">
-        {listaNombres}
-      </div>
+
+      <h3>Carrito de Compras</h3>
+      <div className="lista_Salida">{listaNombres}</div>
     </div>
   );
 };
